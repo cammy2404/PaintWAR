6c53502c60d2f2100414c0ba72d09f303ff0724e
stevenmars <37410353+stevenmars@users.noreply.github.com>
2019-02-08 14:03:15 +0000

    Add files via upload

---
+++ historyForm.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace PaintWAR
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.playerSearch = new System.Windows.Forms.TextBox();
+            this.btn_find_player = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Franklin Gothic Medium", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.SandyBrown;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(822, 67);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Game History";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.Tan;
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.InsetDouble;
+            this.tableLayoutPanel1.ColumnCount = 6;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.Controls.Add(this.label7, 5, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label6, 4, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 181);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 118F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 118F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(775, 114);
+            this.tableLayoutPanel1.TabIndex = 5;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Tan;
+            this.label7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label7.Font = new System.Drawing.Font("Franklin Gothic Medium", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(646, 3);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(123, 118);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Grid Size";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Tan;
+            this.label6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label6.Font = new System.Drawing.Font("Franklin Gothic Medium", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(518, 3);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(119, 118);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Winner";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Tan;
+            this.label5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label5.Font = new System.Drawing.Font("Franklin Gothic Medium", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(390, 3);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(119, 118);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Player 2";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Tan;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label4.Font = new System.Drawing.Font("Franklin Gothic Medium", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(262, 3);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(119, 118);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Player 2 Score";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Tan;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Font = new System.Drawing.Font("Franklin Gothic Medium", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(134, 3);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(119, 118);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Player 1 Score";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Tan;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Font = new System.Drawing.Font("Franklin Gothic Medium", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 118);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Player 1";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.AllowDrop = true;
+            this.comboBox5.BackColor = System.Drawing.Color.Tan;
+            this.comboBox5.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.comboBox5.Font = new System.Drawing.Font("Franklin Gothic Medium", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Items.AddRange(new object[] {
+            "Sort Ascending",
+            "Sort Descending"});
+            this.comboBox5.Location = new System.Drawing.Point(656, 139);
+            this.comboBox5.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(131, 29);
+            this.comboBox5.TabIndex = 19;
+            this.comboBox5.SelectedIndexChanged += new System.EventHandler(this.comboBox5_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Tan;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 298);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(798, 385);
+            this.textBox1.TabIndex = 20;
+            // 
+            // playerSearch
+            // 
+            this.playerSearch.BackColor = System.Drawing.Color.Tan;
+            this.playerSearch.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.playerSearch.Font = new System.Drawing.Font("Franklin Gothic Medium", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playerSearch.ForeColor = System.Drawing.Color.Black;
+            this.playerSearch.Location = new System.Drawing.Point(13, 138);
+            this.playerSearch.Margin = new System.Windows.Forms.Padding(4);
+            this.playerSearch.Name = "playerSearch";
+            this.playerSearch.Size = new System.Drawing.Size(508, 34);
+            this.playerSearch.TabIndex = 21;
+            // 
+            // btn_find_player
+            // 
+            this.btn_find_player.BackColor = System.Drawing.Color.Tan;
+            this.btn_find_player.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_find_player.Font = new System.Drawing.Font("Franklin Gothic Medium", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_find_player.Location = new System.Drawing.Point(529, 138);
+            this.btn_find_player.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_find_player.Name = "btn_find_player";
+            this.btn_find_player.Size = new System.Drawing.Size(119, 36);
+            this.btn_find_player.TabIndex = 22;
+            this.btn_find_player.Text = "Search";
+            this.btn_find_player.UseVisualStyleBackColor = false;
+            this.btn_find_player.Click += new System.EventHandler(this.btn_find_player_Click);
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.Tan;
+            this.label8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label8.Font = new System.Drawing.Font("Franklin Gothic Medium", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(12, 99);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(125, 31);
+            this.label8.TabIndex = 23;
+            this.label8.Text = "Search Player";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.Color.Tan;
+            this.label9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label9.Font = new System.Drawing.Font("Franklin Gothic Medium", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(656, 99);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(131, 35);
+            this.label9.TabIndex = 24;
+            this.label9.Text = "Sort Grid Size";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(822, 778);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.btn_find_player);
+            this.Controls.Add(this.playerSearch);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox5);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form4";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "v";
+            this.Load += new System.EventHandler(this.Form4_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox playerSearch;
+        private System.Windows.Forms.Button btn_find_player;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
---
+++ historyForm.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace PaintWAR
+{
+    public partial class Form4 : Form
+    {
+        
+
+        public Form4()
+        {
+            InitializeComponent();
+            printToLeaderboard();
+
+        }
+
+        public void printToLeaderboard()
+        {
+            int lineCount = File.ReadLines("History.csv").Count();
+
+            Console.WriteLine("Number of lines " + lineCount + 1);
+
+            try
+            {
+                var line = File.ReadAllLines("History.csv");
+
+                for (int i = lineCount; i > -1; i--)
+                {
+
+                    string lines = line.ElementAtOrDefault(i-1); // null if there are less lines
+                    
+                    string[] words = lines.Split(',');                   
+
+                    foreach (string word in words)
+                    {
+                        Console.WriteLine("WORD: " + word);
+                        //write the lie to console window
+                        textBox1.Text += word;
+                        textBox1.Text += "\t";
+
+                    }
+                    textBox1.Text += "\r\n";
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
+
+        public void sortLeaderboard(string userInput)
+        {
+            int lineCount = File.ReadLines("History.csv").Count();
+
+            Console.WriteLine("Number of lines " + lineCount + 1);
+
+            Console.WriteLine("WORKS-------------------------");
+
+            try
+            {
+                var line = File.ReadAllLines("History.csv");
+
+                for (int i = lineCount; i > -1; i--)
+                {
+
+                    string lines = line.ElementAtOrDefault(i - 1); // null if there are less lines
+
+                    if (lines.Contains(userInput))
+                    {
+                        string[] words = lines.Split(',');
+
+                    
+                        foreach (string word in words)
+                        {
+                            Console.WriteLine("WORD: " + word);
+                            //write the lie to console window
+                            string print = word + "\t";
+                            textBox1.Text += print;
+                            
+
+                        }
+                        textBox1.Text += "\r\n";
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
+
+        private void Form4_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox5_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            string gridSize;
+            if (comboBox5.SelectedItem == "Sort Ascending")
+            {
+                gridSize = "30x30";
+                sortLeaderboard(gridSize);
+                gridSize = "40x40";
+                sortLeaderboard(gridSize);
+                gridSize = "50x50";
+                sortLeaderboard(gridSize);
+                gridSize = "60x60";
+                sortLeaderboard(gridSize);
+                gridSize = "70x70";
+                sortLeaderboard(gridSize);
+
+            }
+            else if (comboBox5.SelectedItem == "Sort Descending")
+            {
+                gridSize = "70x70";
+                sortLeaderboard(gridSize);
+                gridSize = "60x60";
+                sortLeaderboard(gridSize);
+                gridSize = "50x50";
+                sortLeaderboard(gridSize);
+                gridSize = "40x40";
+                sortLeaderboard(gridSize);
+                gridSize = "30x30";
+                sortLeaderboard(gridSize);
+
+            }
+            else
+            {
+
+            }
+        }
+
+
+        
+
+        
+
+        private void btn_find_player_Click(object sender, EventArgs e)
+        {
+
+            if (playerSearch.Text == "")
+            {
+                MessageBox.Show("You must enter a name to search.", "Player Name Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            else
+            {
+                textBox1.Clear();
+                sortLeaderboard(playerSearch.Text);
+            }
+        }
+
+        
+    }
+}
+    
+
+
+
